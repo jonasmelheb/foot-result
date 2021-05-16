9c0b34f80346de4d333cb3a09bff3f80404f19e3
melheb.younes <melheb.younes@gmail.com>
2021-05-16 21:05:25 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { MatchListComponent } from './resources/matches/match-list/match-list.component';
+import { MatchDetailComponent } from './resources/matches/match-detail/match-detail.component';
+import { appRoutes } from './app.routes';
+imports: [BrowserModule, RouterModule.forRoot(appRoutes)];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MatchListComponent,
+    MatchDetailComponent,
   ],
   imports: [
     BrowserModule
